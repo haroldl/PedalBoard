4224f1441c533f5bdc2b4d3d365bd1bf9dff9b85
Harold Lee <harold@hotelling.net>
2017-03-18 08:40:48 -0700

    Untested code for MIDI mode, work-in-progress.

--- PedalBoard.ino
+++ PedalBoard.ino
@@ -27,6 +27,9 @@
  *
  * Sample MIDI project for reference:
  * https://www.arduino.cc/en/Tutorial/MidiDevice
+ *
+ * N.B. Keyboard and Serial don't work together, so to support typing as a USB keyboard
+ *      we don't get to use e.g. Serial.println to debug.
  */
 
 #include <frequencyToNote.h>
@@ -40,13 +43,34 @@
 
 int currentMode = KEYB_MODE;
 
+// One octave + the next C.
+// We'll wire up pins 0-12 for the inputs.
+#define NUM_PEDALS 13
+
+// Probably seen as MIDI channel 4 on the synth/computer side
+const byte midiChannel = 3;
+
+// How loud - hard coded since these are on/off switches; [0, 127]
+const byte midiVelocity = 64;
+
+const uint8_t pedalInputs[NUM_PEDALS] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+
+const byte notePitches[NUM_PEDALS] = {
+  pitchC3,  pitchD3b, pitchD3, pitchE3b, pitchE3,
+  pitchF3,  pitchG3b, pitchG3, pitchA3b, pitchA3,
+  pitchB3b, pitchB3,  pitchC3
+  };
+
+// Using INPUT_PULLUP for these, so 0 means the pedal is down, 1 means the pedal is up.
+int pedalValues[NUM_PEDALS] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
 /*
  * selectPin - an input used at setup time to select MIDI or Keyboard mode
  *
  * The Keyboard and MIDIUSB libraries for output both use the USB connection
  * so we want to select one mode or the other at setup time. To select MIDI
  * mode, hold down the selectPin input button and reset the Arduino board.
- * By default, we start in Keyboard mode.Hello, world!
+ * By default, we start in Keyboard mode.
  */
 int selectPin = 2;
 
@@ -73,13 +97,51 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
   if (currentMode == KEYB_MODE) {
-    Keyboard.println("Hello, world!\n");
-    Keyboard.println(currentMode);
-    delay(10000); // 10 sec
+    loopKeyboard();
   } else {
-    // Check the input pins for the pedals and send appropriate MIDI events for each.
-    delay(10000); // 10 sec
+    loopMIDI();
   }
 }
+
+// The loop function for Keyboard mode.
+void loopKeyboard() {
+  Keyboard.println("Hello, world!\n");
+  Keyboard.println(currentMode);
+  delay(10000); // 10 sec
+}
+
+// The loop function for MIDI mode.
+void loopMIDI() {
+  // Check the input pins for the pedals and send appropriate MIDI events for each.
+  for (int i = 0; i < NUM_PEDALS; i++) {
+    int pedalValue = digitalRead(pedalInputs[i]);
+    if (pedalValues[i] != pedalValue) {
+      if (pedalValue == 0) {
+        // The pedal just went down.
+        startNote(notePitches[i]);
+      } else {
+        // The pedal just came up.
+        endNote(notePitches[i]);
+      }
+    }
+    pedalValues[i] = pedalValue;
+  }
+
+  delay(10);
+}
+
+// Send a MIDI event to start playing the note with the given pitch.
+void startNote(byte pitch) {
+  byte velocity = midiVelocity;
+  midiEventPacket_t noteOn = {0x09, 0x90 | midiChannel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+// Send a MIDI event to stop playing the note with the given pitch.
+void endNote(byte pitch) {
+  byte velocity = 0;
+  midiEventPacket_t noteOff = {0x08, 0x80 | midiChannel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
